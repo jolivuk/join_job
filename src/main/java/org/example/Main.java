5759052a43c3ad6f20340640315cf6738f7523fb
USER <jolivuk@gmail.com>
2024-06-03 12:43:57 +0200

    class RubberArray: method append, method appendRange

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,14 @@ import org.taskRubberArray.RubberArray;
 public class Main {
     public static void main(String[] args) {
         RubberArray rubberArray = new RubberArray();
+
+        rubberArray.append(2);
+        rubberArray.append(3);
+        rubberArray.append(5);
+
+        rubberArray.appendRange(new int[]{3,4,5});
+        rubberArray.appendRange(99,45,5);
+
         System.out.println(rubberArray);
     }
 }
--- src/main/java/org/taskRubberArray/RubberArray.java
+++ src/main/java/org/taskRubberArray/RubberArray.java
@@ -1,22 +1,40 @@
 package org.taskRubberArray;
 
-import java.util.Arrays;
-
 public class RubberArray {
+
     private int size;
     private int[] array;
 
     public RubberArray() {
+        this.array = new int[0];
         this.size = 0;
-        this.array = new int[this.size];
+    }
+
+
+
+    public void append (int item){
+        this.size++;
+        int[] newArray = new int[this.size];
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+        newArray[this.size-1] = item;
+        this.array = newArray;
+    }
+
+    public void appendRange (int ... items){
+        for (int i = 0; i < items.length; i++) {
+            append(items[i]);
+        }
     }
 
     @Override
     public String toString() {
-        String res = "";
-        for (int i = 0; i < array.length; i++) {
-            res += String.valueOf(array[i]) + " ";
+        String strArray = "";
+        for (int num: array)
+        {
+            strArray += num + " ";
         }
-        return "[" + res.trim() + "]";
+        return "[" + strArray.trim() + "]";
     }
 }
